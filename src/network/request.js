aff3467a6b3b84f5749e6bc614539c553b646ce9
Sun-QiXin <15153869872@163.com>
2020-05-07 13:12:39 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   //1、创建Axios的实例
   const instance = Axios.create({
     //公共url前缀(接口)
-    baseURL: 'http://152.136.185.210:8000/api/n3',
+    baseURL: '数据接口地址，获取接口见文档提示',
     //超时时间
     timeout: 10000
   });
